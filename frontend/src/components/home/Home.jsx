c14448bc828034d9edf15469608ed35e768b6e0b
REEKRAJROY <reekrajroy@gmail.com>
2024-07-21 20:21:29 +0530

    BE+FE

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+const Home = () => {
+  return (
+    <div className="home d-flex justify-content-center align-items-center">
+      <div className="container d-flex justify-content-center align-items-center flex-column ">
+        <h1 className="text-center">
+          Schedule your work easily.
+        </h1>
+        <p>
+          Simply click on the cta and see demo.<br/>
+          Login to get your progress recorded.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
